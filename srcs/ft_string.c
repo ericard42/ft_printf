44541c0c2591805b3a674a3e7e29e33a1c0952c2
ericard <ericard@student.42.fr>
2020-09-06 17:43:43 +0200

    add flags for char

--- srcs/ft_string.c
+++ srcs/ft_string.c
@@ -6,7 +6,7 @@
 /*   By: ericard <ericard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/21 16:55:34 by ericard           #+#    #+#             */
-/*   Updated: 2020/09/06 15:54:27 by ericard          ###   ########.fr       */
+/*   Updated: 2020/09/06 17:31:32 by ericard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,9 +36,12 @@ int     ft_char(t_flags flags, va_list va)
 
     ret = 1;
     if (flags.minus == 1)
-        return (1);
-    ft_putchar(va_arg(va, int));
-    return (ret);
+        ft_putchar(va_arg(va, int));
+    if (flags.width > ret)
+        ft_width_space(flags.width - ret);
+    if (flags.minus == 0)
+        ft_putchar(va_arg(va, int));
+    return ((flags.width > ret) ? flags.width : ret);
 }
 
 int     ft_strings(t_flags flags, const char *format, size_t i, va_list va)
