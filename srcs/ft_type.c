8fb1e07c958b1b72269d561869e39be26694016c
ericard <ericard@student.42.fr>
2020-09-11 14:29:24 +0200

    add %i

--- srcs/ft_type.c
+++ srcs/ft_type.c
@@ -6,7 +6,7 @@
 /*   By: ericard <ericard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/03 00:51:13 by ericard           #+#    #+#             */
-/*   Updated: 2020/09/06 20:56:32 by ericard          ###   ########.fr       */
+/*   Updated: 2020/09/11 12:46:36 by ericard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,9 @@ int    ft_type(t_flags flags, char c, va_list va)
     ret = 0;
     if(c == 'c' || c == 's' || c == '%')
         ret += ft_strings(flags, c, va);
-    else if (c == 'd')
+    else if (c == 'd' || c == 'i')
         ret += ft_decimal(flags, va);
     else if(c == 'p')
-        ret += 1;
-    else if (c == 'i')
         ret += 1;
     else if (c == 'u' || c == 'x' || c == 'X')
         ret += 1;
