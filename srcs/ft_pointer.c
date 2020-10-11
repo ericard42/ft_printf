1da4816f657d4972547fb72cd553525fb3a9e853
ericard <ericard@student.42.fr>
2020-10-11 17:46:41 +0200

    correction on pointers

--- srcs/ft_pointer.c
+++ srcs/ft_pointer.c
@@ -6,21 +6,21 @@
 /*   By: ericard <ericard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/21 16:56:09 by ericard           #+#    #+#             */
-/*   Updated: 2020/10/09 12:14:10 by ericard          ###   ########.fr       */
+/*   Updated: 2020/10/11 17:41:46 by ericard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int     hexa_null(t_flags flags, char *print)
+int     hexa_null_dot(t_flags flags)
 {
     if (flags.minus == 1)
-        ft_putstr(print, 5);
-    if (flags.width > 5)
-        ft_print_space(flags.width - 5);
+        ft_putstr("0x", 2);
+    if (flags.width > 2)
+        ft_print_space(flags.width - 2);
     if (flags.minus == 0)
-        ft_putstr(print, 5);
-    return (flags.width > 5 ? flags.width : 5);
+        ft_putstr("0x", 2);
+    return (flags.width > 2 ? flags.width : 2);
 }
 
 int     ft_pointer(t_flags flags, va_list va)
@@ -30,21 +30,21 @@ int     ft_pointer(t_flags flags, va_list va)
     unsigned long int   nbr;
 
     nbr = va_arg(va, unsigned long int);
-    if (!nbr)
-        return (hexa_null(flags, ft_strdup("(nil)")));
+	if (!nbr && flags.dot == 1)
+		return (hexa_null_dot(flags));
     print = ft_itoa_base(16, nbr, 'a');
     ret = ft_strlen(print) + 2;
     if (flags.minus == 1)
     {       
         ft_putstr("0x", 2);
-        ft_putstr(print, ret - 2);
+		ft_putstr(print, ret - 2);
     }
     if (flags.width > ret)
         ft_print_space(flags.width - ret);
     if (flags.minus == 0)
     {
         ft_putstr("0x", 2);
-        ft_putstr(print, ret - 2);
+		ft_putstr(print, ret - 2);
     }
     return (flags.width > ret ? flags.width : ret);
 }
